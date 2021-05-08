239dae27b6bef767d2be9dbaceb2651f7af860aa
muhiasamuel <smuhia622@gmail.com>
2021-05-08 08:09:23 +0300

    1223

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -152,7 +152,7 @@
                                              <div class="col-6">
                                                 <div class="bg-dark p-10 text-white text-center">
                                                    <i class="fa fa-plus m-b-5 font-16"></i>
-                                                   <h5 class="m-b-0 m-t-5">120</h5>
+                                                   <h5 class="m-b-0 m-t-5">20</h5>
                                                    <small class="font-light">New Users</small>
                                                 </div>
                                             </div>
@@ -166,24 +166,18 @@
                                              <div class="col-6 m-t-15">
                                                 <div class="bg-dark p-10 text-white text-center">
                                                    <i class="fa fa-tag m-b-5 font-16"></i>
-                                                   <h5 class="m-b-0 m-t-5">9540</h5>
+                                                   <h5 class="m-b-0 m-t-5">20</h5>
                                                    <small class="font-light">Subscriptions</small>
                                                 </div>
                                             </div>
                                             <div class="col-6 m-t-15">
                                                 <div class="bg-dark p-10 text-white text-center">
                                                    <i class="fa fa-table m-b-5 font-16"></i>
-                                                   <h5 class="m-b-0 m-t-5">100</h5>
+                                                   <h5 class="m-b-0 m-t-5">10</h5>
                                                    <small class="font-light">Pending Subscriptions</small>
                                                 </div>
                                             </div>
-                                            <div class="col-6 m-t-15">
-                                                <div class="bg-dark p-10 text-white text-center">
-                                                   <i class="fa fa-globe m-b-5 font-16"></i>
-                                                   <h5 class="m-b-0 m-t-5">8540</h5>
-                                                   <small class="font-light">Online Orders</small>
-                                                </div>
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                     <!-- column -->
